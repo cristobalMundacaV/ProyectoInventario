9552071e1a662a9ffd9e25c9460b889c6f8dcaa7
Cristóbal Mundaca <cristobal.mundacav@gmail.com>
2026-01-06 14:52:46 -0300

    Proyecto base con modelos y Admin en todas las tablas.

--- auditoria/admin.py
+++ auditoria/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Actividad
 
-# Register your models here.
+
+@admin.register(Actividad)
+class ActividadAdmin(admin.ModelAdmin):
+    list_display = ('fecha_hora', 'usuario', 'tipo_accion', 'descripcion')
+    list_filter = ('tipo_accion', 'usuario', 'fecha_hora')
+    search_fields = ('usuario__username', 'descripcion')
+    readonly_fields = ('fecha_hora',)
+    ordering = ('-fecha_hora',)
+
+
+# Auto-register remaining models in this app (preserves custom registrations)
+from django.apps import apps
+app_models = apps.get_app_config('auditoria').get_models()
+for model in app_models:
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
